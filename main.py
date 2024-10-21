178000d501f292906fedf9561ae74347e514cde8
garfieldno1 <75411444+garfieldno1@users.noreply.github.com>
2024-10-21 11:52:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 # YOUR CODE HERE
+n = int(input())
+lst1 = []
+lst2 = []
+for i in range(n):
+    user_input = int(input())
+    lst1.append(user_input)
+for i in range(n):
+    user_input = int(input())
+    lst2.append(user_input)
+summarize = []
+def summation(n):
+    for i in range(n):
+        summarize.append(lst1[i]+lst2[i])
+    return summarize 
+def find_min_max():
+    min = summarize[0]
+    max = summarize[0]
+    for i in range (len(summarize)):
+        if summarize[i] > max:
+            max = summarize[i]
+        elif summarize[i] < min:
+            min = summarize[i]
+    return min,max
+
+print(summation(n))
+print(find_min_max())
